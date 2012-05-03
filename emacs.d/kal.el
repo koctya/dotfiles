3434002c9c81d76c9d3f813fe5fa497ae8a43d4b
Kurt Landrus <klandrus@gmail.com>
2012-05-03 13:28:37 -0400

    corrected load error, updated gc alias'

--- emacs.d/kal.el
+++ emacs.d/kal.el
@@ -11,7 +11,7 @@
 
 ;; Turn off mouse interface early in startup to avoid momentary display
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -l))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 
